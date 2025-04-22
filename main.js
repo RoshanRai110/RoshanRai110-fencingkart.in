df0728f0f8ace324f4e92b93c6163d7c1775679d
Roshan Rai <ggi2021.1480@ggi.ac.in>
2025-04-22 15:07:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+/*================= SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    // Validate that varibles exists
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We ADD  THE SHOW MENU CLASS TO THE DIV TAG WITH THE nav_menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+/*===========slider-img-change============*/
+
+    let images = [
+        "Image/png-img/fen-2.png",
+        "Image/png-img/fen-3.png",
+        "Image/png-img/fen-4.png",
+        "Image/png-img/fen-5.png",
+        "Image/png-img/fen-6.png",
+        "Image/png-img/fen-7.png",
+        "Image/png-img/fen-8.png",
+        "Image/png-img/fen-9.png",
+       
+        
+    ];
+
+    let currentImageIndex = 0;
+    let imgElement = document.getElementById("home-img");
+
+    function changeImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        imgElement.src = images[currentImageIndex];
+    }
+
+    setInterval(changeImage, 3000); // Change image every 3 seconds
+
+
+/*====================== REMOVE MENU ==================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // WHEN WE CLICK ON EACH nav_link, WE REMOVE THE SHOW MENU CLASS
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*================ CHANGE BG COLOR ================*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    //WHEN THE SCROLL IS GREATER THAN 200VH, ADD THE SCROLL-HEADER CLASS TO THE HEADER TAG
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+/*================ SHOW SCROLL TOP =================*/
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    //when the scroll is higher than 560vh, add the show -scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+/*================ SCROLL REVEAL ================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal(
+    `.home_data, .home_img,
+    .about_data, .about_img,
+    .services_content, .menu_img,
+    .app_data, .app_img,
+    .contact_data, .contact_button,
+    .footer_content`, {
+        interval: 200
+})
